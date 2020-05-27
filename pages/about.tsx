380686aed2003d615bed30bba01ba703b921df3e
Rafael Paz <rafael.paz@everestate.com>
2020-05-27 15:15:31 +0200

    test

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,7 +11,7 @@ const AboutPage: React.FunctionComponent = () => {
 
   return (
     <Layout title="About | Next.js + TypeScript Example">
-      <h1>About</h1>
+      <h1>About!!!</h1>
       <p>This is the about page</p>
       <p>
         <Link href="/">
